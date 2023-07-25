0c6a6c33f613ca1a642b9aa457f95903e1a3c843
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-07-26 02:01:19 +0500

    000 + 001

---
+++ HomeworkAssignment_000/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameOutput
+{
+    internal class NameOutput
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя плиз: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите символ: ");
+            char symbol = Console.ReadKey().KeyChar;
+
+            string middleLine = $"{symbol}{name}{symbol}";
+            string frame = string.Empty;
+
+            for (int i = 0; i < middleLine.Length; i++)
+            {
+                frame += symbol;
+            }
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine(frame);
+            Console.WriteLine(middleLine);
+            Console.WriteLine(frame);
+        }
+    }
+}
